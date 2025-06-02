3f970a3d3fb9939a774bdf185c7c3ae8994fd562
KETEMAASIRES <ketemaasires@gmail.com>
2025-06-01 22:13:23 -0700

    off

--- src/Utility/FireBase.jsx
+++ src/Utility/FireBase.jsx
@@ -12,13 +12,14 @@ import "firebase/compat/firestore";
 //   appId: "1:942028226040:web:70fa6cdcff71f8d465ea0d",
 // };
 const firebaseConfig = {
-  apiKey: "AIzaSyAoWWLU32SVnbjblBmEMPSPfuWvzeLPXKE",
-  authDomain: "clone-b3c11.firebaseapp.com",
-  projectId: "clone-b3c11",
-  storageBucket: "clone-b3c11.firebasestorage.app",
-  messagingSenderId: "942028226040",
-  appId: "1:942028226040:web:70fa6cdcff71f8d465ea0d"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
 export const auth = firebase.auth(app);
